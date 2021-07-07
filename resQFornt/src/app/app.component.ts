1907a8ab219e9ce5499f200048aa03d301885e82
pummarin.phiaswsoongnern@priorsolution.co.th <pummarin.phiaswsoongnern@priorsolution.co.th>
2021-07-07 19:43:14 +0700

    booking res

--- resQFornt/src/app/app.component.ts
+++ resQFornt/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit} from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MessageService } from './service/message.service';
 
 @Component({
@@ -6,13 +8,24 @@ import { MessageService } from './service/message.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dailytemperature';
-  input: any;
-  constructor(public messageService: MessageService) {}
+  input: any ;
+  name:any;
+  number:any;
+  
+  constructor(public messageService: MessageService,public dialog: MatDialog,private modalService: NgbModal) {}
+ 
+  ngOnInit(): void {
+    // this.open();
+  }
+
+  
 
   sendMessage() {
     if (this.input) {
+      console.log(this.input);
+      
       this.messageService.sendMessage(this.input);
       this.input = '';
     }
@@ -20,4 +33,41 @@ export class AppComponent {
   toggleTodo(id: number){
     this.messageService.toggleTodo(id);
   }
+
+  open() {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = 'World';
+  }
 }
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class NgbdModalContent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
+// export class NgbdModalComponent {
+//   constructor(private modalService: NgbModal) {}
+
+//   open() {
+//     const modalRef = this.modalService.open(NgbdModalContent);
+//     modalRef.componentInstance.name = 'World';
+//   }
+// }
+
+
--- resQFornt/src/app/app.module.ts
+++ resQFornt/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
